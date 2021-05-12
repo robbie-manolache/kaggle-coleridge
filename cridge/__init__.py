3b61f7069269b202bef1240ac235da4d8f0d48cf
robbie29 <37903975+robbie29@users.noreply.github.com>
2021-05-12 21:57:30 +1000

    set up key helper files and config for the competition

---
+++ cridge/__init__.py
@@ -0,0 +1,6 @@
+
+# support functions
+from cridge.helpers.config import env_config
+from cridge.lazykaggler.competitions import competition_download, \
+    competition_files, competition_list
+from cridge.lazykaggler.kernels import kernel_output_download
